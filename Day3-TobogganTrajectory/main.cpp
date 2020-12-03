6a359292415d4e6a969ba95d0ceb2761aa82eb30
Dominik <dominik.leko3@gmail.com>
2020-12-03 10:06:20 +0100

    Day 3, Part 2

--- Day3-TobogganTrajectory/main.cpp
+++ Day3-TobogganTrajectory/main.cpp
@@ -11,9 +11,9 @@ using std::getline;
 using std::cout;
 using std::endl;
 
-int check_slope(vector<vector<char>> geology, size_t xDiff, size_t yDiff) {
+long long check_slope(vector<vector<char>> geology, size_t xDiff, size_t yDiff) {
     size_t line_length = geology[0].size();
-    int tree_encounters = 0;
+    long long tree_encounters = 0;
     for (size_t x = 0, y = 0; y < geology.size(); x = (x + xDiff) % line_length, y += yDiff) {
         if (geology[y][x] == '#') tree_encounters++;
         else if (geology[y][x] == '.') continue;
@@ -38,10 +38,14 @@ int main() {
         geology.push_back(vector(line.begin(), line.end()));
     }
 
-    int tree_encounters = check_slope(geology, 3, 1);
-
     // do some calculating
+    long long tree_encounters_1 = check_slope(geology, 1, 1);
+    long long tree_encounters_2 = check_slope(geology, 3, 1);
+    long long tree_encounters_3 = check_slope(geology, 5, 1);
+    long long tree_encounters_4 = check_slope(geology, 7, 1);
+    long long tree_encounters_5 = check_slope(geology, 1, 2);
+    long long result = tree_encounters_1 * tree_encounters_2 * tree_encounters_3 * tree_encounters_4 * tree_encounters_5;
 
     // write output
-    cout << "tree encounters: " << tree_encounters << endl;
+    cout << "tree encounters: " << result << endl;
 }
