f58290284c7300be2881c093c947850ae9794692
Dominik <dominik.leko3@gmail.com>
2020-12-11 18:31:48 +0100

    Day 11, Part 2.

--- Day11-SeatingSystem/main.cpp
+++ Day11-SeatingSystem/main.cpp
@@ -17,22 +17,6 @@ enum SeatState { Floor, Empty, Occupied };
 
 using WaitingArea = vector<vector<SeatState>>;
 
-ostream &operator<<(ostream& output, SeatState seat_state) {
-    if (seat_state == Floor) output << '.';
-    else if (seat_state == Empty) output << 'L';
-    else output << '#';
-    return output;
-}
-
-ostream &operator<<(ostream& output, WaitingArea wa) {
-    for (auto row : wa) {
-        for (auto seat_state : row)
-            output << seat_state;
-        output << endl;
-    }
-    return output;
-}
-
 SeatState parse_seat(char c) {
     if (c == '.') return Floor;
     else if (c == 'L') return Empty;
@@ -43,21 +27,57 @@ bool in_bounds(size_t lb, size_t ub, size_t val) {
     return lb <= val && val <= ub;
 }
 
-int n_of_occ_surrounding_seats(WaitingArea& wa, size_t i, size_t j) {
-    int n_occupied = 0;
-    for (size_t i_ = 0; i_ <= 2; i_++)
-        for (size_t j_ = 0; j_ <= 2; j_++) {
-            size_t i_cur = i + i_ - 1;
-            size_t j_cur = j + j_ - 1;
-            if (!(in_bounds(0, wa.size() - 1, i_cur) && in_bounds(0, wa[0].size() - 1, j_cur))) continue;
-            else if (i_ == 1 && j_ == 1) continue;
-            else if (wa[i_cur][j_cur] == Occupied) n_occupied++;
+bool in_bounds(WaitingArea& wa, pair<size_t, size_t> pos) {
+    return in_bounds(0, wa.size() - 1, pos.first)
+        && in_bounds(0, wa[0].size() - 1, pos.second);
+}
+
+// returns whether there is a seat in sight.
+// "false" means, that there is no seat in the waiting area in the line of sight
+template<class F>
+bool look_at(WaitingArea& wa, pair<size_t, size_t> pos, F func, SeatState& next_seat) {
+    pos = func(pos);
+    while (in_bounds(wa, pos)) {
+        if (wa[pos.first][pos.second] == Floor) {
+            pos = func(pos);
+            continue;
+        } else if (wa[pos.first][pos.second] == Empty) {
+            next_seat = Empty;
+            return true;
+        } else {
+            next_seat = Occupied;
+            return true;
         }
-    
+    }
+    return false;
+}
+
+const vector<pair<size_t, size_t>> directions{
+    {-1, -1},
+    {-1, 0},
+    {-1, 1},
+    {0, 1},
+    {1, 1},
+    {1, 0},
+    {1, -1},
+    {0, -1}
+};
+
+int n_of_occ_surrounding_seats(WaitingArea& wa, pair<size_t, size_t> pos) {
+    int n_occupied = 0;
+
+    SeatState next_seat;
+    for (auto direction : directions) {
+        if (look_at(
+            wa, pos, [direction](auto pos){return pair<size_t, size_t>{pos.first + direction.first, pos.second + direction.second}; }, next_seat)
+            && next_seat == Occupied)
+            n_occupied++;
+    }
+
     return n_occupied;
 }
 
-// returns, whether the wa has changed
+// returns, whether the waiting area has changed
 bool do_turn(WaitingArea& wa) {
     vector<pair<size_t, size_t>> to_occupied;
     vector<pair<size_t, size_t>> to_empty;
@@ -65,11 +85,14 @@ bool do_turn(WaitingArea& wa) {
 
     for (size_t i = 0; i < wa.size(); i++) {
         for (size_t j = 0; j < wa[0].size(); j++) {
-            if (wa[i][j] == Empty && n_of_occ_surrounding_seats(wa, i, j) == 0) {
+            int n_occupied = n_of_occ_surrounding_seats(wa, {i, j});
+            if (wa[i][j] == Empty && n_occupied == 0) {
                 to_occupied.push_back({i, j});
                 will_change = true;
             }
-            else if (wa[i][j] == Occupied && n_of_occ_surrounding_seats(wa, i, j) >= 4) {
+            else if (wa[i][j] == Occupied && n_occupied >= 5) {
+                if (i == 1 && j == 0)
+                    cout << "n_occupied: " << n_occupied << endl;
                 to_empty.push_back({i, j});
                 will_change = true;
             }
@@ -111,9 +134,11 @@ int main() {
     }
 
     // do some calculating
+    
     while(do_turn(wa)) {}
     int n_occupied = number_of_occupied_seats(wa);
 
     // write output
+    
     cout << "number of occupied seats: " << n_occupied << endl;
 }
