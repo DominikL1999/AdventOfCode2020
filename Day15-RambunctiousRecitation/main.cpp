31537a7a2d43f5c101c536cda0d8fafc04067f34
Dominik <dominik.leko3@gmail.com>
2021-01-05 16:58:01 +0100

    Day 15, first working version.

--- Day15-RambunctiousRecitation/main.cpp
+++ Day15-RambunctiousRecitation/main.cpp
@@ -2,41 +2,95 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <cassert>
 
 using std::ifstream;
 using std::vector;
+using std::map;
 using std::string;
 using std::pair;
 using std::getline;
 using std::cout;
 using std::endl;
 
+const size_t NUMBER_OF_ROUNDS = 2020;
+
+template<typename K, typename V>
+bool contains(const map<K, V>& map, K key) {
+    return map.find(key) != map.end();
+}
+
+void make_line() {
+    cout << "------------------------------" << endl;
+}
+
+// returns the next number to be said
+size_t say_number(map<size_t, pair<size_t, size_t>>& last_said, size_t number, size_t pos) {
+
+    // step 1: get next_number
+
+    size_t next_number;
+    pair<size_t, size_t>& p = last_said[number];
+    if (p.first == SIZE_MAX) {
+        next_number = 0;
+    } else {
+        next_number = p.second - p.first;
+    }
+
+    // step 2: update last_said
+    // auto it = last_said.find(next_number);
+    // if (it == last_said.end()) {
+    //     it->second.first = SIZE_MAX;
+    //     it->second.second = pos;
+    // } else {
+    //     it->second.first = it->second.second;
+    //     it->second.second =
+    // }
+    if (!contains(last_said, next_number))
+        last_said[next_number] = {SIZE_MAX, pos};
+    else {
+        pair<size_t, size_t>& p2 = last_said[next_number];
+        p2.first = p2.second;
+        p2.second = pos;
+    }
+
+    // step 3: return next_number
+    return next_number;
+}
+
 int main() {
     // parse input
 
     ifstream input("input/input.txt");
     // ifstream input("input/test-input.txt");
-    vector<size_t> numbers;
+
+    map<size_t, pair<size_t, size_t>> last_said;
+    size_t pos = 0;
+    size_t number;
     while (input.good()) {
         string substr;
         getline(input, substr, ',');
-        numbers.push_back(stoi(substr));
-    }
+        number = stoi(substr);
 
+        last_said[number] = {SIZE_MAX, pos};
+        pos++;
+    }
+    
+    // for (auto pair : last_said)
+    //     cout << pair.first << ": " << pair.second.first << " " << pair.second.second << endl;
+    // make_line();
+    
     // do some calculating
-    for (size_t i = numbers.size(); i < 2020; i++) {
-        size_t last_spoken_number = numbers[numbers.size() - 1];
-        size_t last_spoken_pos = SIZE_MAX;
-        for (size_t j = numbers.size() - 2; j != SIZE_MAX; j--) {
-            if (numbers[j] == last_spoken_number) {
-                last_spoken_pos = j; 
-                break;
-            }
-        }
-        if (last_spoken_pos == SIZE_MAX) numbers.push_back(0);
-        else numbers.push_back(numbers.size() - 1 - last_spoken_pos);
+    for (; pos < NUMBER_OF_ROUNDS; pos++) {
+        number = say_number(last_said, number, pos);
+
+        // for (auto pair : last_said)
+        //     cout << pair.first << ": " << pair.second.first << " " << pair.second.second << endl;
+        // cout << "number after: " << number << endl;
+        // make_line();
     }
 
     // write output
-    cout << "last number: " << numbers[numbers.size() - 1] << endl;
+    cout << "last number: " << number << endl;
 }
